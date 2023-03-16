cb7874140148465be5332a21ab55b1b2afaf2293
Farid Ahmed <faridahmed.tech@gmail.com>
2023-03-16 21:28:17 +0530

    implement view product on hover feature

--- src/components/ProductsGrid/ProductsGrid.tsx
+++ src/components/ProductsGrid/ProductsGrid.tsx
@@ -14,7 +14,9 @@ const ProductsGrid: FC = () => {
         <div className={styles.container}>
             <div className={styles.product}>
                 <h2 onClick={event => handleLike(event)} id="1">♡</h2>
-                <div className={styles.view}></div>
+                <div className={styles.view}>
+                    <h3>View Product</h3>
+                </div>
                 <img src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFzaGlvbnxlbnwwfHwwfHw%3D&w=1000&q=80" />
                 <h4>Round neck cotton tee</h4>
                 <p><del>Rs. 599</del> <b>Rs.549</b></p>
